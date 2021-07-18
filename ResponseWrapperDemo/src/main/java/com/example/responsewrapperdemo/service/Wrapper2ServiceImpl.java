28f543b9ba9dd4ce43540ff33ee4fc32fd07b396
Emil <einasyrov@vtb.ru>
2021-07-18 16:26:03 +0300

    delete postconstruct

--- ResponseWrapperDemo/src/main/java/com/example/responsewrapperdemo/service/Wrapper2ServiceImpl.java
+++ ResponseWrapperDemo/src/main/java/com/example/responsewrapperdemo/service/Wrapper2ServiceImpl.java
@@ -7,19 +7,12 @@ import lombok.NonNull;
 import ru.emilnasyrov.lib.response.wrapper.IWrapperService;
 import ru.emilnasyrov.lib.response.wrapper.annotation.WrapperService;
 
-import javax.annotation.PostConstruct;
-
 /**
  * @author Emil Nasyrov (Emilikan)
  */
 
 @WrapperService(wrapperModel = Wrapper2.class)
 public class Wrapper2ServiceImpl implements IWrapperService<MainModel, Passport> {
-
-    @PostConstruct
-    public void init() {
-        System.out.println("4");
-    }
 
     @Override
     public Passport getData(@NonNull MainModel body) {
